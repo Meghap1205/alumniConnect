a1e3a2439221e7bf7e88bd37feaf0bbbf0afa7f4
Amisha <amishajha0426@gmail.com>
2024-05-16 20:54:20 +0530

    added student signup page functionality

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/server': {
+        target: "http://localhost:3000",
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
-})
+});
--- server/models/student.model.js
+++ server/models/student.model.js
@@ -27,7 +27,6 @@ const studentSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  
   contact: {
     type: Number,
     required: true,
