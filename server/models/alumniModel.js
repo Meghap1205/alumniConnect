67d5a2428a6b31fb6dea205ca24f5f0ad0a5ae3a
Meghap1205 <meghajpatel1205@gmail.com>
2024-05-28 09:58:23 +0530

    alumni model

---
+++ server/models/alumniModel.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose");
+
+const alumniSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    alumniID: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    dateOfBirth: {
+        type: Date,
+        required: true,
+    },
+    address: {
+        type: String,
+        required: true,
+    },
+    contact: {
+        type: Number,
+        required: true,
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+    profilePicture: {
+        type: String,
+        default:
+            "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+    },
+    jobTitle: {
+        type: String,
+        required: true,
+    },
+    company: {
+        type: String,
+        required: true,
+    },
+    startDate: {
+        type: Date,
+        required: true,
+    },
+    role: {
+        type: String,
+        default: "",
+    },
+
+});
+
+const Alumni = mongoose.model("Alumni", alumniSchema);
+
+module.exports = Alumni;
